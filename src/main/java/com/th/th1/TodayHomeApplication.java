6e3df7751a0001d4a0697974e676dea97412188c
yijin <87287021+JeonYijin@users.noreply.github.com>
2021-11-12 14:20:43 +0900

    Initial commit

---
+++ src/main/java/com/th/th1/TodayHomeApplication.java
@@ -0,0 +1,13 @@
+package com.th.th1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayHomeApplication.class, args);
+	}
+
+}
