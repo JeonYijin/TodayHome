5ad5f98eefa7b72f348d69307e2c410cd92b33dc
yijin <87287021+JeonYijin@users.noreply.github.com>
2021-11-18 16:34:20 +0900

    sns 로그인
    kakao, facebook, naver로그인 api 연결

---
+++ src/main/java/com/th/th1/sns/FacebookVO.java
@@ -0,0 +1,89 @@
+package com.th.th1.sns;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class FacebookVO implements OAuth2User {
+
+	private String username;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+	private Boolean enabled;
+	private String social;
+	
+	private Map<String, Object> attributes;
+	
+	
+	public void setAttributes(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
+	@Override
+	public Map<String, Object> getAttributes() {
+		// TODO Auto-generated method stub
+		return this.attributes;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+	      authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+		return authorities;
+	}
+	
+	
+	
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getSocial() {
+		return social;
+	}
+	public void setSocial(String social) {
+		this.social = social;
+	}
+	
+	
+	
+}
