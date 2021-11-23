86084dccabd7b87387ea7122a17417f01d56834b
abcbcdyl <86539734+abcbcdy@users.noreply.github.com>
2021-11-23 11:59:40 +0900

    Update memberJoin.js

--- src/main/webapp/resources/js/memberJoin.js
+++ src/main/webapp/resources/js/memberJoin.js
@@ -21,26 +21,64 @@ let memberId = '';
 	
 	
 	
-	/*let manual = manual + '<input class="form-control" placeholder="입력해주세요" size="1" value="">'
+	/*let manual = '<input class="form-control" placeholder="입력해주세요" size="1" value="">'
 	manual = manual + ' <button class="email-input__domain__expand active" aria-label="초기화" type="button" tabindex="-1">'
 	manual = manual + '<svg class="icon" width="10" height="10" preserveAspectRatio="xMidYMid meet" style="fill: currentcolor;">'
 	manual = manual + '<path fill-rule="evenodd" d="M5 4L8.5.3l1 1.1L6.2 5l3.5 3.6-1 1L5 6.1 1.4 9.6l-1-1L3.9 5 .4 1.5l1.1-1L5 3.8z">'
 	manual = manual + '</path></svg></button></span>'*/
 
+
+
 	$("#email-domain-select").change(function(){
 		
-		//이메일 직접입력 - 나중에 해결
+		//이메일 직접입력
 		if($(this).val() == 'manual') {
 			
-			$(this).parent().innerHTML = manual;
+			$('#email-domain-select').hide();
+			$('#email-domain-manual').attr("type", "text");
 			
+			$("#email-domain-manual").change(function(){
+				emailDomain = $('#email-domain-manual').val();
+				memberId = emailId + '@' + emailDomain;
+				$("#member-id").val(memberId);
+		});
+			
+		} else {
+			$('#email-domain-manual').hide();
+			emailDomain = $(this).val();
+			memberId = emailId + '@' + emailDomain;
+			$("#member-id").val(memberId);
 		}
 		
-		emailDomain = $(this).val();
-		memberId = emailId + '@' + emailDomain;
-		$("#member-id").val(memberId);
 	});
 	
+		
+	
+
+/*이메일 인증*/
+$(".emailAuthBtn").click(function() {// 메일 입력 유효성 검사
+		var mail = $("#member-id").val(); //사용자의 이메일 입력값. 
+		
+		if (mail == "") {
+			alert("메일 주소가 입력되지 않았습니다.");
+		} else {
+			//mail = mail+"@"+$(".domain").val(); //셀렉트 박스에 @뒤 값들을 더함.
+			$.ajax({
+				type : 'post',
+				url : '/CheckMail',
+				data : {
+					mail:mail
+					},
+				dataType :'json',
+
+			});
+			alert("인증번호가 전송되었습니다.") 
+			isCertification=true; //추후 인증 여부를 알기위한 값
+		}
+	});
+
+
+
 
 /*비밀번호 확인*/
 $('.pwcheck').blur(function(){
@@ -54,7 +92,29 @@ $('.pwcheck').blur(function(){
 
 /*약관 동의*/
 
+//전체 동의 클릭 시 체크, 해제
+$('.checkbox-agreeAll').click(function(){
+	 var checked = $(this).is(":checked");
+	 if(checked) {
+		$('.checks').prop("checked", true);
+	}else {
+		$('.checks').prop("checked", false);
+	}
+});
 
+//약관 모두 클릭 시 전체 동의 
+$('.checks').click(function(){
+	let result = true;
+	
+	$('.checks').each(function(index, item){
+		if(!$(this).prop("checked")) {
+			result = false;
+		}
+	});
+	
+	alert(result);	
+	$('.checkbox-agreeAll').prop("checked", result);
+})
 
 
 
@@ -115,6 +175,24 @@ $('.essential-nm').on({
 		}
 	}
 });
+
+
+//회원가입 버튼
+$('#joinBtn').on('click', function(){
+	
+	
+	if($("#member-id").val() == null) {
+		alert('필수항목을 입력해주세요');
+		var location = document.querySelector("#email-id").offsetTop;
+		window.scrollTo({top:location, behavior:'auto'});
+		
+	}else if($('#password').val() == null) {
+		var location = document.querySelector("#password").offsetTop;
+		window.scrollTo({top:location, behavior:'auto'});
+	}
+	
+})
+
 
 
 
