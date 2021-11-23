6dff4aabefca28a0eed5f60ba65f6c0da78e5dde
shaymee <acteasily@gmail.com>
2021-11-23 12:01:03 +0900

    1

--- src/main/java/com/th/th1/home/HomeController.java
+++ src/main/java/com/th/th1/home/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping("/")
-	public String getIndex() throws Exception{
+	public String getIndex() throws Exception {
 		return "index";
 	}
+}	
